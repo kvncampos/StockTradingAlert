8a0dd093acb76d3841b8402275341df625f8a12f
kvncampos <kvnelpdfw@hotmail.com>
2023-11-04 22:33:13 -0500

    Added SNMP Functionality

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import json
 import yfinance as yahooFinance
 from datetime import datetime
+from smtplib import *
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from creds import GMAIL_SERVER, FROM_ADDRESS, TO_ADDRESS, PASSWORD, SUBJECT
+
 
 get_10_year_treasury_yield_data = yahooFinance.Ticker("^TNX")
 
@@ -10,9 +16,6 @@ end_date = datetime.now().strftime('%Y-%m-%d')
 # Valid options are 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y and ytd.
 last_week_data = get_10_year_treasury_yield_data.history(period=last_week, interval='1d')
 last_6_months_data = get_10_year_treasury_yield_data.history(period=last_3_months, interval='1wk')
-
-# ----------------- TICKER INFO -----------------------
-print(f'TICKER: 10 Year Treasury Yield')
 
 # ----------------- TICKER INFO TODAY -----------------------
 close_price = last_week_data
@@ -35,16 +38,24 @@ price_diff_today_vs_yesterday = format(float(current_closing_price) - float(prev
 percentage_change = format((float(current_closing_price) - float(previous_day_closing_price)) /
                            float(previous_day_closing_price) * 100, '.4f')
 
+
 # ----------------- TICKER INFO OUTPUT -----------------------
+print(f'TICKER: 10 Year Treasury Yield')
+
 print(
     f"------------------------------\n"
     f"--- Today's Closing Price: {today_date} -> \N{Dollar Sign}{current_closing_price}\n"
     f"--- Yesterday's Closing Price: {yesterday_date} -> \N{Dollar Sign}{previous_day_closing_price}"
 )
+
+good_vs_bad_day = None
 if float(price_diff_today_vs_yesterday) < 0:
+    good_vs_bad_day = "Price Has Gone Down! \N{White Smiling Face}"
     print(f"Price Has Gone Down! \N{White Smiling Face}")
+
     print(f"----------------------")
 else:
+    good_vs_bad_day = "Price Has Gone Up! \N{White Frowning Face}"
     print(f"Price Has Gone Up! \N{White Frowning Face}")
     print(f"----------------------")
 print(f"--- Difference in Price Movement (Today vs Yesterday): \N{Dollar Sign}{price_diff_today_vs_yesterday}")
@@ -63,13 +74,71 @@ print(last_6_months_data[['Open', 'Close']])
 
 # ----------------- Latest 3 News Articles -----------------------
 latest_3_news = get_10_year_treasury_yield_data.get_news()[:3]
+news_dict = {}
 print("\n\nLatest 3 News Articles About the 10 Year Treasury Yield.")
 for num, each_dict in enumerate(latest_3_news, start=1):
     for key, value in each_dict.items():
         if key == 'title':
             print(f"  {num}) Title of Article: {value}")
+            news_dict[f'{key.title()}-{num}'] = value
         if key == 'link':
             print(f"  LinkURL: {value}")
+            news_dict[f'{key.title()}-{num}'] = value
     print()
 
+# pp(news_dict)
+# --------------------------- SNMP VARIABLES -----------------------------------
+
+GMAIL_SERVER = GMAIL_SERVER
+FROM_ADDRESS = FROM_ADDRESS
+TO_ADDRESS = TO_ADDRESS
+PASSWORD = PASSWORD
+SUBJECT = SUBJECT
+CURRENT_TIME = datetime.now()
+NEWS = json.dumps(news_dict, indent=4)
+NEWS_ARTICLES = ''
+for key, value in news_dict.items():
+    NEWS_ARTICLES += f'{key}: {value}\n\n'
+
+# --------------------------- SNMP MESSAGE SETUP -----------------------------------
+# EMAIL MESSAGE STRUCTURE IN HTML
+MESSAGE = f"""
+<h2>
+TICKER: 10 Year Treasury Yield<br>
+-------------------------------------
+</h2>
+<p>
+--- Today's Closing Price: {today_date} -> \N{Dollar Sign}{current_closing_price}<br>
+--- Yesterday's Closing Price: {yesterday_date} -> \N{Dollar Sign}{previous_day_closing_price}<br>
+{good_vs_bad_day}
+----------------------<br>
+--- Difference in Price Movement (Today vs Yesterday): \N{Dollar Sign}{price_diff_today_vs_yesterday}<br>
+--- Percentage Change: {percentage_change}%<br>
+---------------------------------------------------------------<br>
+Latest 3 News Articles About the 10 Year Treasury Yield.<br>
+{NEWS_ARTICLES}<br>
+</p>
+<br>
+<b>Powered by www.pythonanywhere.com!</b><br>
+<b>Ran on {CURRENT_TIME} </b>
+"""
+
+
+# Send Email with Rain Status for the Day
+with SMTP(GMAIL_SERVER, port=587) as connection:
+    connection.starttls()
+    connection.login(FROM_ADDRESS, PASSWORD)
+
+    msg = MIMEMultipart()
+
+    msg['From'] = FROM_ADDRESS
+    msg['To'] = TO_ADDRESS
+    msg['Subject'] = SUBJECT
+
+    # add in the message body
+    msg.attach(MIMEText(MESSAGE, 'html'))
+    connection.send_message(msg)
+
+    print(msg)
+    connection.quit()
 
